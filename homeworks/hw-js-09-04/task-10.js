f47bd47988d190dea9a65d36bf6de360d03296e3
Victor <vict02r05@gmail.com>
2021-04-12 23:42:30 +0300

    added a third solution

--- homeworks/hw-js-09-04/task-10.js
+++ homeworks/hw-js-09-04/task-10.js
@@ -2,12 +2,24 @@
 
 import users from "./users.js";
 
+//Variant 1
 const getSortedUniqueSkills = (users) =>
   users
     .reduce((acc, { skills }) => [...acc, ...skills], [])
     .filter((element, index, array) => array.indexOf(element) === index)
     .sort();
 
+//Variant 2
+// const getSortedUniqueSkills = (users) =>
+//   users
+//     .reduce((acc, { skills }) => {
+//       acc.push(...skills);
+//       return acc;
+//     }, [])
+//     .filter((element, index, array) => array.indexOf(element) === index)
+//     .sort();
+
+//Variant 3
 // let arrOfSkills = [];
 // const getSortedUniqueSkills = (users) => {
 //   users.forEach((element) => {

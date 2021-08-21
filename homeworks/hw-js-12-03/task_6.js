14a3b9f401a4ee758ea854cdabd03c3f03d07625
Victor <vict02r05@gmail.com>
2021-08-21 21:32:57 +0300

    add

--- homeworks/hw-js-12-03/task_6.js
+++ homeworks/hw-js-12-03/task_6.js
@@ -1,13 +1,13 @@
-" use strict";
+' use strict';
 
 const values = [];
 
-values[0] = prompt("Перше число");
-values[1] = prompt("Друге число");
-values[2] = prompt("Третє число");
+values[0] = prompt('Перше число');
+values[1] = prompt('Друге число');
+values[2] = prompt('Третє число');
 
 let largest = Math.max.apply(Math, values);
 
-if (isNaN(values[0] || isNaN(values[1]) || isNaN(values[2]))) {
-  alert("Error");
+if (isNaN(values[0]) || isNaN(values[1]) || isNaN(values[2])) {
+  alert('Error');
 } else console.log(largest);
